2bfa63c0665eb134db2bc04ece12ad78a7203c38
LensFox <kotyarasmertt@mail.ru>
2021-10-12 17:54:28 +0300

    fixed tests

--- ClientService.UI/clients-service/src/tests/reducer.test.js
+++ ClientService.UI/clients-service/src/tests/reducer.test.js
@@ -14,7 +14,7 @@ describe('when reduce clients', () => {
         {
             id: '3',
             name: 'Client 3'
-        },
+        }
     ];
     describe('when fetch clients', () => {
         const state = undefined;
@@ -28,8 +28,8 @@ describe('when reduce clients', () => {
 
         it('should store all clients', () => {
             expect(actualResult).toEqual(expectedRetult);
-        })
-    })
+        });
+    });
 
     describe('when fetch clients and clients are exist', () => {
         const state = {
@@ -50,49 +50,48 @@ describe('when reduce clients', () => {
 
         it('should rewrite clients', () => {
             expect(actualResult).toEqual(expectedRetult);
-        })
-    })
+        });
+    });
 
     describe('when delete existing client', () => {
         const state = {
             clients
         };
         const action = {
-            type: actions.FETCH_CLIENTS_SUCCEEDED,
+            type: actions.DELETE_CLIENT_SUCCEEDED,
             payload: '1'
         };
 
-        const expectedRetult = [
-            clients[1],
-            clients[2]
-        ];
+        const expectedRetult = [clients[1], clients[2]];
         const actualResult = reducer(state, action).clients;
 
         it('should successfully remove first client', () => {
             expect(actualResult).toEqual(expectedRetult);
-        })
-    })
+        });
+    });
 
     describe('when delete a client which is not exist', () => {
         const state = {
             clients
         };
         const action = {
-            type: actions.FETCH_CLIENTS_SUCCEEDED,
+            type: actions.DELETE_CLIENT_SUCCEEDED,
             payload: '0'
         };
 
         const expectedRetult = clients;
-        const actualResult = reducer(state, action).clients;
+        const reducerRes = reducer(state, action);
+        console.log(reducerRes);
+        const actualResult = reducerRes.clients;
 
         it('state unchanged', () => {
             expect(actualResult).toEqual(expectedRetult);
-        })
-    })
-})
+        });
+    });
+});
 
 describe('1', () => {
     it('2', () => {
-        expect(1 + 1).toEqual(2)
-    })
-})
+        expect(1 + 1).toEqual(2);
+    });
+});
